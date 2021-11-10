1bd80ae38f56da0f3f07c50190520e189ea4997e
Jesse Taube <Mr.Bossman075@gmail.com>
2021-11-09 21:16:45 -0500

    bruh

--- minner.py
+++ minner.py
@@ -5,8 +5,8 @@ import time
 import psutil
 import multiprocessing
 import math
-deviation = 0.5
-always_free_cores  = 1.5
+deviation = 1
+always_free_cores  = 2.3
 
 
 cores = multiprocessing.cpu_count()
@@ -20,18 +20,20 @@ def minewith(mining):
 	global proc
 	if proc != None:
 		proc.kill()
-	proc = subprocess.Popen(['/bin/ccminer', '-a', 'verus', '-o','stratum+tcp://na.luckpool.net:3956', '-u', 'RXhFKA9cTJXRbK8nuuZ3aCVDyQvC96dRLD.ja_02', '-p', 'x', '-t' ,  str(mining)])
+	proc = subprocess.Popen(['/bin/ccminer', '-a', 'verus', '-o','stratum+tcp://na.luckpool.net:3956', '-u', 'RXhFKA9cTJXRbK8nuuZ3aCVDyQvC96dRLD.ja_02', '-p', 'x', '-t' ,  str(mining)],stdout=subprocess.DEVNULL)
 while True:
 	free_cores = (100-psutil.cpu_percent())/(100/cores)
-	if  always_free_cores+deviation < free_cores:
+	if always_free_cores-deviation >  free_cores:
+		killM()
+		print("Recalculating cores")
+		time.sleep(10)
+		mining_cores = 0
+	elif  always_free_cores+deviation < free_cores:
 		tmp = mining_cores + math.floor(free_cores-math.ceil(always_free_cores))
 		if tmp < (cores-always_free_cores ) and mining_cores != tmp:
+			if mining_cores == 0:
+				free_cores -= tmp
 			mining_cores = tmp
 			minewith(mining_cores)
 			print("There are "+ str(round(free_cores,2)) + " free cores. Mining on " + str(mining_cores)+ " of " + str(cores) + " cores.")
-	elif always_free_cores-deviation >  free_cores:
-		killM()
-		time.sleep(10)
-		mining_cores = 0
-		print("Recalculating cores")
 	time.sleep(1)
