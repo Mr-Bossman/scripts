e1757c9c2bfd43a01148303aa1b4937528178fe6
Jesse Taube <Mr.Bossman075@gmail.com>
2021-11-09 21:02:22 -0500

    bruh

--- minner.py
+++ minner.py
@@ -13,12 +13,14 @@ cores = multiprocessing.cpu_count()
 mining_cores = 0
 proc = None
 def killM():
+	global proc
 	if proc != None:
 		proc.kill()
 def minewith(mining):
+	global proc
 	if proc != None:
 		proc.kill()
-	proc = subprocess.Popen(['/bin/ccminer', '-a', 'verus', '-o','stratum+tcp://na.luckpool.net:3956', '-u', 'RXhFKA9cTJXRbK8nuuZ3aCVDyQvC96dRLD.ja_02', '-p', 'x', '-t' ,  str(Mining)])
+	proc = subprocess.Popen(['/bin/ccminer', '-a', 'verus', '-o','stratum+tcp://na.luckpool.net:3956', '-u', 'RXhFKA9cTJXRbK8nuuZ3aCVDyQvC96dRLD.ja_02', '-p', 'x', '-t' ,  str(mining)])
 while True:
 	free_cores = (100-psutil.cpu_percent())/(100/cores)
 	if  always_free_cores+deviation < free_cores:
