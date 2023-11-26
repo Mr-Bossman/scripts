492d8a3af240fb7ed346845b8cb0bad5ad861b28
Jesse Taube <Mr.Bossman075@gmail.com>
2023-11-25 19:51:22 -0500

    gruopps

--- setup_pc/install.sh
+++ setup_pc/install.sh
@@ -3,10 +3,14 @@ groups | grep sudo &>/dev/null || echo "Run \`su root -c \"usermod -aG sudo $(wh
 mkdir /tmp/setup_pc/
 cd /tmp/setup_pc/
 sudo apt-add-repository -ync non-free contrib non-free-firmware
+sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install git
 sudo apt install -y $(cat packages)
 sudo apt autoremove
+
+sudo usermod -aG tty $(whoami)
+sudo usermod -aG libvirt $(whoami)
 
 wget -nc "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O chrome.deb
 wget -nc "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
